3ace47887e71378376cebedbf3f86d99d559ccd2
Vivien Takács <takacsvivienbuda@gmail.com>
2020-02-13 17:20:22 +0100

    fixing rename board

--- data_manager.py
+++ data_manager.py
@@ -22,23 +22,6 @@ def get_cards_for_board(cursor, id_):
     return cards
 
 
-@database_common.connection_handler
-def alma(cursor, new, alma):
-    cursor.execute('''
-        
-        INSERT INTO board_statuses (board,status, status_id)
-        VALUES (1,%(new)s, %(alma)s);
-    ''', {"new": new, "alma":alma})
-
-@database_common.connection_handler
-def alma2(cursor, id_):
-    cursor.execute('''
-        SELECT status FROM board_statuses
-        WHERE board = %(id_)s;
-    ''', {"id_": id_})
-    cards = cursor.fetchall()
-
-    return cards
 
 @database_common.connection_handler
 def get_board_statuses(cursor, board_id):
